073c8e42ae4146295daa06f7ab6e2bcab1e03c35
Yolanda Morris <y.mijoe7145@gmail.com>
2022-03-24 22:09:45 -0400

    Initial Commit

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Task() {
   return (
-    <div className="task">
+    <div className="task" >
       <div className="label">CATEGORY HERE</div>
       <div className="text">TEXT HERE</div>
       <button className="delete">X</button>
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { TASKS } from "../data";
+//import Task from "./Task.js";
 
-function TaskList() {
-  return (
-    <div className="tasks">
-      {/* display a list of tasks using Task component */}
-    </div>
+//const tasks = {TASKS}
+
+
+
+const TaskList = () =>  {
+  let tasks = [TASKS];
+  console.log(tasks)
+
+  return  (
+    <div className="tasks"> 
+      <h1>Where are the tasks?</h1> 
+      <p>
+        tasks
+      </p>
+      
+    {/* display a list of tasks using Task component */}  
+   </div>
   );
 }
+
 
 export default TaskList;
